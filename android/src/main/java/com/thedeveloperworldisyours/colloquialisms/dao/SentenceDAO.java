e5b90778758c2700e0f76fb0a313df46a47021d3
Cabezas <javiergonzalezcabezas@gmail.com>
2015-05-30 02:23:06 +0100

    added android folder

---
+++ android/src/main/java/com/thedeveloperworldisyours/colloquialisms/dao/SentenceDAO.java
@@ -0,0 +1,17 @@
+package com.thedeveloperworldisyours.colloquialisms.dao;
+
+import android.content.Context;
+
+import com.thedeveloperworldisyours.colloquialisms.models.Sentence;
+import com.thedeveloperworldisyours.colloquialisms.utils.Constants;
+
+import garin.artemiy.sqlitesimple.library.SQLiteSimpleDAO;
+
+/**
+ * Created by javiergonzalezcabezas on 23/5/15.
+ */
+public class SentenceDAO extends SQLiteSimpleDAO<Sentence> {
+    public SentenceDAO(Context context) {
+        super(Sentence.class, context, Constants.DATABASE_NAME);
+    }
+}
---
+++ android/src/main/java/com/thedeveloperworldisyours/colloquialisms/views/CustomTextView.java
@@ -0,0 +1,42 @@
+package com.thedeveloperworldisyours.colloquialisms.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.thedeveloperworldisyours.colloquialisms.MainApplication;
+import com.thedeveloperworldisyours.colloquialisms.R;
+
+/**
+ * Created by javiergonzalezcabezas on 25/5/15.
+ */
+public class CustomTextView extends TextView {
+
+        public CustomTextView(Context context, AttributeSet attrs, int defStyle) {
+            super(context, attrs, defStyle);
+            init(attrs);
+        }
+
+        public CustomTextView(Context context, AttributeSet attrs) {
+            super(context, attrs);
+            init(attrs);
+
+        }
+        public CustomTextView(Context context) {
+            super(context);
+            init(null);
+        }
+
+        private void init(AttributeSet attrs) {
+            if (attrs!=null) {
+                TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.TextViewForm);
+                //String fontName = a.getString(R.styleable.EditTextForm_fontName);
+                //if (fontName!=null) {
+                setTypeface(MainApplication.Fonts.PENCIL);
+                //}
+                a.recycle();
+            }
+        }
+    }
+
